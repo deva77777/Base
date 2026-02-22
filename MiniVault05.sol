2993967c4cc6982bfd3a64ebad6725feade9f85e
deva77777 <dpbhatt18@gmail.com>
2026-02-22 08:53:40 +0530

    create mintvault.sol

---
+++ MiniVault05.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract MiniVault05 {
+    mapping(address => uint256) public balances;
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Insufficient");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+}
